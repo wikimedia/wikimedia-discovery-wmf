a32e61390d95b182f06efd3c53597ac91bce395c
Mikhail Popov <mikhail@mpopov.com>
2015-08-26 16:03:42 -0700

    Fixed the calculations, added more documentation + features

--- R/power.R
+++ R/power.R
@@ -1,65 +1,107 @@
+oddsRatio <- function(p_treatment, p_control) {
+  return( (p_treatment/(1-p_treatment)) / (p_control/(1-p_control)) )
+}
+pTreatment <- function(p_control, odds_ratio) {
+  return( (odds_ratio*p_control) / ((p_control*(odds_ratio-1)) + 1) )
+}
+pControl <- function(p_treatment, odds_ratio) {
+  return( 1/( (odds_ratio * ((1/p_treatment) - 1)) + 1 ) )
+}
+
 #'@title calculate sample size given odds ratio
 #'
-#'@param precision Relative precision dictates your margin of error. Optional
-#'  only if \code{pa} is provided.
 #'@param odds_ratio The expected odds ratio. That is, the ratio of the odds of
-#'  the outcome in the test group (b) relative to the control group (a).
-#'@param p_a Expected prevalence of outcome in the control group (a). Optional
-#'  only if \code{precision} is provided.
+#'  the outcome in the test group relative to the control group. Optional,
+#'  but see \strong{Details}.
+#'@param p_control Your guess for prevalence of outcome in the control group.
+#'  Optional but see \strong{Details}.
+#'@param p_treatment Your guess for prevalence of outcome in the test group.
+#'  Optional but see \strong{Details}.
+#'@param power The ability of the test to detect an effect where there is one.
+#'  Power = 1 - Prob(Type 2 error). Optional. See \strong{Value} for details.
 #'@param conf_level Desired confidence level. Defaults to 95\%.
 #'@param sample_ratio Ratio of test group to control group. 1 is even split.
-#'@param visualize Whether to plot relative precision or prevalence of outcome
-#'  in the control group vs sample size. Can be used to help make a decision.
+#'@param visualize Whether to plot power or prevalence of outcome in the
+#'  control group vs sample size. Can be used to help make a decision.
 #'
-#'@return If \code{pa} was not provided, returns a data frame containing
-#'  possible values of \eqn{\pi_a} and the appropriate sample size.
-#'  If \code{pa} was provided, returns a single sample size estimate.
+#'@details The function only needs to know two of the following three:
+#' \code{odds_ratio}, \code{p_control}, and \code{p_treatment}. If given
+#' all three, it will check to make sure the odds ratio is correct. It
+#' will figure out the missing third value from the other two.
+#'
+#'@return If \code{power} was not provided, returns vector containing
+#'  possible power values and the appropriate sample size for each \%.
+#'  If all values were provided, returns a single sample size estimate.
+#'
+#'@section References:
+#'Wang, H., Chow, S.-C., & Li, G. (2002). On sample size calculation based on
+#'  odds ratio in clinical trials. \emph{Journal of Biopharmaceutical
+#'  Statistics}, \strong{12}(4), 471–483.
+#'  \url{http://doi.org/10.1081/BIP-120016231}
+#'
+#'@examples
+#'sample_size_odds(p_treatment = 0.4, p_control = 0.25, power = 0.8)
+#'sample_size_odds(odds_ratio = 2, p_control = 0.4, power = c(0.8, 0.9, 0.95))
+#'sample_size_odds(odds_ratio = 2, p_control = 0.4)
+#'sample_size_odds(odds_ratio = 2, p_control = 0.4, visualize = TRUE)
 #'
 #'@export
-sample_size_odds <- function(odds_ratio, precision = NULL, p_a = NULL, conf_level = 0.95, sample_ratio = 1, visualize = FALSE) {
-  if (is.null(precision) && is.null(p_a)) stop("'precision' and 'p_a' cannot both be missing.")
-  precision_missing <- is.null(precision)
-  prevalence_missing <- is.null(p_a)
-  if (precision_missing) {
-    if (p_a >= 1 || p_a < 0) stop("'p_a' must be in [0, 1)")
-    if ((p_a * odds_ratio) + 1 == 1) stop("p_a * odds_ratio + 1 cannot be equal to 1")
-    precision <- seq(0.01, 0.25, 0.005)
-  } else if (prevalence_missing) {
-    if (precision <= 0 && precision >= 1) stop("'precision' must be in (0, 1)")
-    p_a <- seq(0.05, 0.95, 0.01)
+sample_size_odds <- function(odds_ratio = NULL,
+                             p_control = NULL,
+                             p_treatment = NULL,
+                             power = NULL, conf_level = 0.95,
+                             sample_ratio = 1, visualize = FALSE)
+{ # Checks
+  power_missing <- is.null(power)
+  pC_missing <- is.null(p_control)
+  pT_missing <- is.null(p_treatment)
+  oR_missing <- is.null(odds_ratio)
+  if ( (oR_missing + pC_missing + pT_missing) > 1)
+    stop("Only one of {odds_ratio, p_control, p_treatment} can be missing.")
+
+  # Imputations (Part 1)
+  if (power_missing) {
+    power <- seq(0.5, 0.99, 0.01)
   }
-  p_b <- round((odds_ratio * p_a) / (p_a * (odds_ratio - 1) + 1), 3)
-  x <- 1/(p_b * (1 - p_b) * sample_ratio)
-  y <- 1/(p_a * (1 - p_a))
-  z <- abs(qnorm((1-conf_level)/2))
-  n_a <- ceiling((1/x + 1/y)*((z^2)/(log10(1 - precision))^2))
-  n_b <- ceiling(sample_ratio * n_a)
-  n <- n_a + n_b
+  # Imputations (Part 2)
+  if (pC_missing) {
+    p_control <- pControl(p_treatment, odds_ratio)
+  } else if (pT_missing) {
+    p_treatment <- pTreatment(p_control, odds_ratio)
+  } else if (oR_missing) {
+    odds_ratio <- oddsRatio(p_treatment, p_control)
+  }
+
+  # Calculations
+  x <- p_treatment * (1 - p_treatment) * sample_ratio
+  y <- p_control * (1 - p_control)
+  z_alpha <- qnorm((1-conf_level)/2)
+  z_beta <- qnorm(1-power)
+  n_b <- (1/x + 1/y) * (((z_alpha + z_beta)^2) / (log(odds_ratio)^2))
+  n_a <- sample_ratio * n_b
+  n <- ceiling(n_a + n_b)
+
+  # Visualization
   if (visualize) {
-    if (prevalence_missing) {
-      plot(p_a, n, type = "l", main = "Sample size as function of prevalence in controls",
-           ylab = "N", xlab = "Prevalence of outcome in control group", lwd = 2, xaxt = "n")
-      axis(side = 1, at = seq(0.05, 0.95, 0.1), labels = sprintf("%.0f%%", 100*seq(0.05, 0.95, 0.1)))
-      abline(v = seq(0.05, 0.95, 0.1), lty = "dotted", col = "lightgray", lwd = par("lwd"))
-    } else if (precision_missing) {
-      plot(precision, n, type = "l", main = "Sample size as function of relative precision",
-           ylab = "N", xlab = "Relative precision of the study", lwd = 2, xaxt = "n")
-      axis(side = 1, at = c(0.01, seq(0.05, 0.25, 0.05)),
-           labels = sprintf("%.0f%%", 100*c(0.01, seq(0.05, 0.25, 0.05))))
-      abline(v = c(0.01, seq(0.05, 0.25, 0.05)), lty = "dotted", col = "lightgray", lwd = par("lwd"))
+    if (power_missing || length(power) > 1) {
+      plot(power, n, type = "l", main = "Sample size as function of statistical power",
+           ylab = "N", xlab = "Power to detect effect", lwd = 2, xaxt = "n")
+      axis(side = 1, at = seq(0.5, 1, 0.1),
+           labels = sprintf("%.0f%%", 100*seq(0.5, 1, 0.1)))
+      abline(v = seq(0.5, 1, 0.1), lty = "dotted", col = "lightgray", lwd = par("lwd"))
     } else {
       warning("All parameters known. Nothing to visualize.")
     }
   }
-  if (prevalence_missing) {
-    return(data.frame("prevalence_in_controls" = p_a,
-                      "prevalence_in_test_group" = p_b,
-                      "sample_size" = n))
-  } else if (precision_missing) {
-    return(data.frame("relative_precision" = precision, "sample_size" = n))
+
+  # Output
+  if (power_missing || length(power) > 1) {
+    names(n) <- sprintf("%.0f%%", power*100)
+    return(n)
   } else {
-    return(c("sample_size" = n))
+    return(n)
   }
+
 }
 
 #'@title calculate sample size given effect size
@@ -72,30 +114,39 @@ sample_size_odds <- function(odds_ratio, precision = NULL, p_a = NULL, conf_leve
 #'@param sig_level Probability of Type 1 error. Usually called alpha.
 #'  Defaults to 0.05.
 #'@param power Ability to detect the effect. (1 - probability of Type 2 error)
-#'
+#'  Defaults to 80\%.
 #'@return If \code{w} was not provided, returns a data frame containing
 #'  possible values of w and the corresponding sample size estimates.
+#'
+#'@examples
+#'sample_size_effect()
+#'sample_size_effect(0.1)
+#'sample_size_effect(w = 0.1, groups = 3, sig_level = 0.001, power = 0.9)
 #'
 #'@importFrom pwr pwr.chisq.test
 #'@export
 sample_size_effect <- function(w = NULL, groups = 2,
-                               sig_level = 0.05, power = 0.95) {
+                               sig_level = 0.05, power = 0.8)
+{ # Checks
   w_missing <- is.null(w)
   if (!w_missing && w <= 0.01) stop("'w' must be > 0.01")
   if (power <= 0.1 || power > 1.0) stop("'power' must be in (0.1, 1]")
+
+  # Imputation
   if (w_missing) w <- c(0.05, 0.1, 0.3, 0.5)
+
+  # Calculation and output
   if (length(w) > 1) {
     n <- ceiling(sapply(w, function(ww) {
       pwr::pwr.chisq.test(w = ww, N = NULL, df = groups - 1,
                           sig.level = sig_level, power = power)$N
     }))
-    return(data.frame("effect_size" = c("tiny", "small", "medium", "large"),
-                      "cohen_w" = w,
-                      "sample_size" = n))
+    names(n) <- c("tiny", "small", "medium", "large")
+    return(n)
   } else {
     n <- ceiling(pwr::pwr.chisq.test(w = w, N = NULL, df = groups - 1,
                                      sig.level = sig_level, power = power)$N)
-    return(c("sample_size" = n))
+    return(n)
   }
 }
 
--- tests/testthat/testSampleSizeCalcs.R
+++ tests/testthat/testSampleSizeCalcs.R
@@ -1,30 +1,28 @@
 context("Sample size calculations")
 
 test_that("sample_size_odds returns the appropriate estimates", {
-  expect_equal(sample_size_odds(1.1, 0.01, 0.45, 0.95, 1), c("sample_size" = 200356))
-  expect_equal(sample_size_odds(1.1, p_a = 0.45,
-                                conf_level = 0.90)[c(1, 10, 20, 40), "sample_size"],
-               c(141110, 4454, 1160, 272))
-  expect_equal(sample_size_odds(1.1, precision = 0.05,
-                                conf_level = 0.90)[c(1, 25, 50, 75, 91), "sample_size"],
-               c(1086, 4578, 5392, 3522, 998))
+  expect_equal(sample_size_odds(odds_ratio = 2, p_treatment = 0.4, p_control = 0.25, power = 0.8, conf_level = 0.95), 311)
+  expect_equal(sample_size_odds(p_treatment = 0.4, p_control = 0.25, power = 0.8), 311)
+  expect_equal(unname(sample_size_odds(p_treatment = 0.4, p_control = 0.25,
+                                       power = c(0.8, 0.9, 0.95))), c(311, 416, 514))
 })
 
 test_that("sample_size_odds returns errors when it should", {
   expect_error(sample_size_odds())
-  expect_error(sample_size_odds(1.1))
-  expect_error(sample_size_odds(precision = 0.01, p_a = 0.45))
+  expect_error(sample_size_odds(2))
+  expect_error(sample_size_odds(odds_ratio = 2, power = 0.8))
 })
 
 test_that("sample_size_odds returns warnings when it should", {
-  expect_warning(sample_size_odds(1.1, 0.01, 0.45, 0.95, 1, visualize = TRUE),
+  expect_warning(sample_size_odds(p_treatment = 0.4, p_control = 0.25, power = 0.8, visualize = TRUE),
                  "All parameters known. Nothing to visualize.")
 })
 
 test_that("sample_size_effect returns the appropriate estimates", {
-  expect_equal(sample_size_effect(0.3), c("sample_size" = 145))
-  expect_equal(sample_size_effect(0.1, groups = 3), c("sample_size" = 1545))
-  expect_equal(sample_size_effect()$sample_size, c(5198, 1300, 145, 52))
+  expect_equal(sample_size_effect(0.3), 88)
+  expect_equal(sample_size_effect(0.1, groups = 3, power = 0.95), 1545)
+  expect_equal(sample_size_effect(),
+               c("tiny" = 3140, "small" = 785, "medium" = 88, "large" = 32))
 })
 
 test_that("sample_size_effect returns errors when it should", {
